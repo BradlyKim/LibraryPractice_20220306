6d3989ed76f62ff6319c4f664554537bf445c9a8
BradlyKim <kjh5864@naver.com>
2022-03-06 11:08:18 +0900

    안드로이드 전화 기능 (Call) 활용  - 앱이 권한문제로 죽는다

--- app/src/main/java/com/neppplus/librarypractice_20220306/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220306/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.librarypractice_20220306
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,6 +15,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents(){
+
+        btnCall.setOnClickListener {
+
+//            임시 : Call 기능 실제 활용 => 앱이 죽을 예정
+
+            val myUri = Uri.parse("tel:1566-7322")
+            val myIntent = Intent( Intent.ACTION_CALL, myUri )
+            startActivity(myIntent)
+
+        }
+
         imgProfile.setOnClickListener {
 
 //            사진을 크게 보는 화면으로 넘어가자.
